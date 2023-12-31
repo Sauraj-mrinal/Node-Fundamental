0b3a6acaa4cca9c4a71f145167146c574694e83e
Sauraj-mrinal <msauraji@gmail.com>
2023-12-31 10:24:49 +0530

    Add

--- Making Basic Server/index.js
+++ Making Basic Server/index.js
@@ -18,15 +18,38 @@ const http = require('http') // http ->the way to requre
 
 // http.createServer()//it will take a function as a parameter and listen 
  http.createServer((req, res)=>{
-    res.write('Hello');
+    res.write('Mrinal Sauraj');
     res.end();
-}).listen(3000)// port number
+}).listen(4000)// port number
 // or 
 // http.createServer(fun).listen(4200)
 // let fun = function(request, response){
 //     response.write(" hello this is mrinal")
 //     response.end();
 // }
+Node.js can handle a large number of concurrent requests due to its event-driven,
+ non-blocking nature. Several factors contribute to its scalability:
+
+Asynchronous I/O:
+
+Node.js can efficiently handle a large number of requests simultaneously without waiting for each operation to complete.
+Event-Driven Architecture:
+
+The event loop efficiently manages and distributes tasks, allowing Node.js to handle multiple connections concurrently.
+Single Thread with Event Loop:
+
+While Node.js runs in a single thread, it offloads I/O-bound tasks to the operating system, preventing blocking and allowing the server to handle other requests.
+Scalability with Clustering:
+
+Node.js applications can be scaled horizontally by using clustering. Multiple Node.js processes can run in parallel, each handling a portion of the incoming requests.
+
+process.exit is used to terminate a Node.js application. It takes an exit code as an argument. A code of 0 indicates successful termination, 
+while any other code indicates an error or abnormal termination.
+
+
+req.url: Contains the URL string of the incoming HTTP request.
+req.headers: Contains the headers of the incoming HTTP request.
+req.method: Contains the HTTP method (GET, POST, PUT, DELETE, etc.) of the incoming request.
 
 
 
---
+++ Sharpener/Q/index.js
@@ -0,0 +1,55 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET') {
+    // Serve the HTML form on a GET request
+    res.setHeader('Content-Type', 'text/html');
+    res.end(`
+      <form method="post" action="/submit">
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name" required>
+        <br>
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email" required>
+        <br>
+        <button type="submit">Submit</button>
+      </form>
+    `);
+  } else if (req.method === 'POST' && req.url === '/submit') {
+    // Handle form submission on a POST request
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk;
+    });
+
+    req.on('end', () => {
+      // Parse the form data
+      const formData = new URLSearchParams(body);
+      const name = formData.get('name');
+      const email = formData.get('email');
+
+      // Write the form data to a file
+      const data = `Name: ${name}, Email: ${email}\n`;
+      fs.appendFile('formData.txt', data, (err) => {
+        if (err) throw err;
+        console.log('Form data written to formData.txt');
+      });
+
+      // Redirect the user with a 302 response
+      res.writeHead(302, { 'Location': '/' });
+      res.end();
+    });
+  } else {
+    // Handle other requests with a 404 response
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+// Set the server to listen on port 3000
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
