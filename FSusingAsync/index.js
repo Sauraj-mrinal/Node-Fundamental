ec261831e53a970e4fbc30bc9d11ce36956659b7
Sauraj-mrinal <msauraji@gmail.com>
2024-01-03 11:56:46 +0530

    Add

--- FSusingAsync/index.js
+++ FSusingAsync/index.js
@@ -1,13 +1,14 @@
+const { log } = require('console');
 const fs = require('fs')
 
 // i need to cerate a file 
 
-fs.writeFile("read.txt", "today is awesome day 31_12_2023",(err)=>{
+// fs.writeFile("read.txt", "today is awesome day 31_12_2023",(err)=>{
 
 
-    console.log("file is created");
-    console.log(err);
-})
+//     console.log("file is created");
+//     console.log(err);
+// })
 
 // we pass them a function as a argument - a callback - that get  called
 // when task is finished.
@@ -19,3 +20,15 @@ fs.writeFile("read.txt", "today is awesome day 31_12_2023",(err)=>{
 
 
 
+//--------------------------------------------------------------------------------------------------
+// if we want to add more data then
+fs.appendFile('read.txt', 'everyone is celebriting new year party  drinking enjoying but i am trying to learn something node ... god ...........', (err)=>{
+    console.log("task completed");
+})
+
+
+// now i want to read the file 
+fs.readFile('read.txt','UTF-8', (err,data)=>{
+   console.log(data);
+
+})
---
+++ Input from Command Line/index.js
@@ -0,0 +1,28 @@
+//--------------------------------------- 
+
+// Set input from command line 
+// Create file with input 
+// Delete the file with input 
+// interview Question
+
+//----------------------------------------
+
+console.log("Hii ......... this is mrinal");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
