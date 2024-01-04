a685be8374d6b794b822df3a968c5ed49b384c6f
Sauraj-mrinal <msauraji@gmail.com>
2024-01-04 18:33:19 +0530

    Add

--- core_Module_fundamentals/index.js
+++ core_Module_fundamentals/index.js
@@ -17,13 +17,42 @@ console.log(__dirname);// E:\Node\Node-Fundamental\core_Module_fundamentals
 
 
 
+// In Node.js, Modules are the blocks of encapsulated code that communicate with an external 
+// application on the basis of their related functionality
+
+// Modules are of three types:
+
+// Core Modules
+// local Modules
+// Third-party Modules
+
+//----------------------------------------------------------------
+
+// Syntax:
+
+// const module = require('module_name');
+
+//----------------------------------------------------------------
 
 
 
+// Core Modules	                       Description
 
+// 1)http	                   creates an HTTP server in Node.js.
 
+// 2)assert	                   set of assertion functions useful for testing.
 
+// 3)fs	                        used to handle file system.
 
+// 4) path	                    includes methods to deal with file paths
+
+// 5) process	                provides information and control about the current Node.js process.
+
+// 6) os	                    provides information about the operating system.
+
+// 7)querystring	            utility used for parsing and formatting URL query strings.
+
+// 8)url	                     module provides utilities for URL resolution and parsing.
 
 
 
