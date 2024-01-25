91d08e9e57374860dd4b2485a14c31484d99a8a9
Sauraj-mrinal <msauraji@gmail.com>
2024-01-25 07:20:47 +0530

    Add

---
+++ ExpressMiddleware/app.js
@@ -0,0 +1,32 @@
+const http = require('http')
+const port = 3000
+const  express = require('express')
+const app = express();
+
+// here only we can write middleware 
+// after express app calling and befor server call
+
+// so "use () "->  is use to manage middleware
+app.use((req, res,next)=>{
+    console.log('In the first middleware function');
+// to call or continue with second middleware 
+// we write next();
+
+next();
+})
+app.use((req, res, next)=>{
+    console.log("this is second middleware function");
+})
+
+const server = http.createServer(app)
+server.listen(port)
+
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+//------------------------------------------
+// output
+//------------------------------------------
+// In the first middleware function
+// this is second middleware function
