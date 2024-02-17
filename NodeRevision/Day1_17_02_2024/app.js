838d588aea35939410846b97e3cc3c39bc0929cc
msauraji <msauraji@gmail.com>
2024-02-18 00:20:32 +0530

    Add

--- NodeRevision/Day1_17_02_2024/app.js
+++ NodeRevision/Day1_17_02_2024/app.js
@@ -1,6 +1,7 @@
 // now we are going to create a server in node js 
 
 // first we need to import core modules 
+let port =3000;
 const http = require('http');
 
 const server = http.createServer( (req,res)=>{
